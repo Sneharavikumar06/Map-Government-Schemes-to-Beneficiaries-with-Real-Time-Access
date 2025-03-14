dc49f75c610f36b7ded7e04380682965e7e09d35
Sneha Ravikumar <160413325+Sneharavikumar06@users.noreply.github.com>
2025-03-14 12:00:37 +0530

    Create HomePage.js

---
+++ Frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Government Schemes Portal</h1>
+      <p>Find the best schemes tailored for you!</p>
+    </div>
+  );
+};
+
+export default HomePage;
