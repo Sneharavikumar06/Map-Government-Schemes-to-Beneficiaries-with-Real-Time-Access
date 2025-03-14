f37ba7bf39bd810f5ec14c152a356d721a933b99
Sneha Ravikumar <160413325+Sneharavikumar06@users.noreply.github.com>
2025-03-14 11:25:47 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  aadhaar: { type: String, required: true, unique: true },
+  income: { type: Number, required: true },
+  occupation: { type: String, required: true },
+  disability: { type: String },
+  password: { type: String, required: true }
+});
+
+module.exports = mongoose.model("User", UserSchema);
