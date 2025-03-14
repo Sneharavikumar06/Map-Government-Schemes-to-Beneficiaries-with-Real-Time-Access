1a85921dbc9025b63f8c3331e6a0ce0608253188
Sneha Ravikumar <160413325+Sneharavikumar06@users.noreply.github.com>
2025-03-14 19:23:56 +0530

    Update authService.js

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/auth";
 
 export default {
-  register: (data) => axios.post(`${API_URL}/register`, data),
-  login: (data) => axios.post(`${API_URL}/login`, data),
+  register: (data) => axios.post(`${API_URL}/Register`, data),
+  login: (data) => axios.post(`${API_URL}/Login`, data),
 };
